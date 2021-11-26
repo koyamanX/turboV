6d2fdaab018500432bbd5c78640fbfacd0ad33cf
koyamanX <ckoyama.1996@gmail.com>
2021-11-27 04:02:08 +0900

    Add WISHBONE RAM and its unit test

---
+++ src/integration/ram64x8K.h
@@ -0,0 +1,12 @@
+#ifndef RAM64X8K_H
+#define RAM64X8K_H
+
+#include "wishbone_common.h"
+
+declare ram64x8K {
+	func_in reset();
+	WISHBONE_GEN_SLAVE_IF(32, 64, 8)
+	WISHBONE_GEN_SLAVE_TAG_IF()
+}
+
+#endif
