71a050a0e478eefaa7c5f648049aaae47b77c189
koyamanX <ckoyama.1996@gmail.com>
2021-11-26 23:58:43 +0900

    Use WISHBONE global clock and reset names

--- src/wishbone/wishbone_syscon.h
+++ src/wishbone/wishbone_syscon.h
@@ -1,7 +1,9 @@
 #ifndef WISHBONE_SYSCON_H
 #define WISHBONE_SYSCON_H
 
-declare wishbone_syscon {
+declare wishbone_syscon interface {
+	input CLK_I;
+	input RST_I;
 	/* The system clock output which coordinates
 		all activities for the internal logic within
 		the WISHBONE interconnect.
