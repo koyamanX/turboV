d8169b7ec29272e1336ff5148427406b849e45d5
koyamanX <ckoyama.1996@gmail.com>
2021-12-20 23:26:12 +0900

    WI26 fix circleCI workflow

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,13 @@ source image/scripts/env.sh
 
 pushd tests &> /dev/null
 
+ret=0
 for i in *; do 
 	pushd $i &> /dev/null
 		make
+		ret=$((ret+$?))
 	popd &> /dev/null
 done
 		
 popd &> /dev/null
+exit $ret
