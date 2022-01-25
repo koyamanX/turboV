a98126c3489118ffa6a1a419d3434cee0b3c9ca4
koyamanX <ckoyama.1996@gmail.com>
2022-01-26 00:47:14 +0900

    Add reservation station

---
+++ src/core/reservation_station.h
@@ -0,0 +1,54 @@
+#ifndef RESERVATION_STATION_H
+#define RESERVATION_STATION_H
+
+struct reservation_station_t {
+	Busy0;
+	Tag0[4];
+	Op0[7];
+	Vj0[32];
+	Vk0[32];
+	Qj0[6];
+	Qk0[6];
+	Dest0[6];
+	A0[32];
+	Busy1;
+	Tag1[4];
+	Op1[7];
+	Vj1[32];
+	Vk1[32];
+	Qj1[6];
+	Qk1[6];
+	Dest1[6];
+	A1[32];
+#define SIZEOF_RESERVATION_STATION_T 252
+};
+
+declare reservation_station {
+	input Valid0;
+	input Op0[7];
+	input Vj0[32];
+	input Vk0[32];
+	input Qj0[6];
+	input Qk0[6];
+	input Dest0[6];
+	input A0[32];
+	input Valid1;
+	input Op1[7];
+	input Vj1[32];
+	input Vk1[32];
+	input Qj1[6];
+	input Qk1[6];
+	input Dest1[6];
+	input A1[32];
+	output rs_tag[4];
+	func_in issue(Op1, Vj1, Vk1, Qj1, Qk1, Dest1, A1, Op0, Vj0, Vk0, Qj0, Qk0, Dest0, A0): rs_tag;
+	func_in flush();
+	func_in wakeup();
+	func_out full();
+	func_out dispatch();
+	input CDB0Id[6];
+	input CDB0Val[32];
+	func_in CDB0(CDB0Id, CDB0Val);
+}
+
+#endif
