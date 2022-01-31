127b6ad4767d434fab52bb33dd68acf9a4994ec4
koyamanX <ckoyama.1996@gmail.com>
2022-02-01 01:20:05 +0900

    Fix size

--- src/core/reorder_buffer.h
+++ src/core/reorder_buffer.h
@@ -9,26 +9,26 @@ struct reorder_buffer_t {
 	Busy0;
 	PC0[32];
 	Inst0[32];
-	Dest0[4];
+	Dest0[5];
 	Value0[32];
 	Valid1;
 	Busy1;
 	PC1[32];
 	Inst1[32];
-	Dest1[4];
+	Dest1[5];
 	Value1[32];
-#define SIZEOF_REORDER_BUFFER_T 204
+#define SIZEOF_REORDER_BUFFER_T 206
 };
 declare reorder_buffer {
 	input valid0;
 	input pc0[32];
 	input inst0[32];
-	input dest0[4];
+	input dest0[5];
 	input valid1;
 	input pc1[32];
 	input inst1[32];
-	input dest1[4];
-	output tag[2];
+	input dest1[5];
+	output tag[LOG2_ROB_NUM_OF_ENTRIES];
 	func_in push(valid0, pc0, inst0, dest0, valid1, pc1, inst1, dest1): tag;
 	func_in flush();
 	func_out full();
@@ -40,7 +40,7 @@ declare reorder_buffer {
 	input CDB0Id[LOG2_ROB_NUM_OF_ENTRIES];
 	input CDB0Val[32];
 	func_in CDB0(CDB0Id, CDB0Val);
-	input CDB1Id[6];
+	input CDB1Id[LOG2_ROB_NUM_OF_ENTRIES];
 	input CDB1Val[32];
 	func_in CDB1(CDB1Id, CDB1Val);
 	output read_operandA_val[32];
