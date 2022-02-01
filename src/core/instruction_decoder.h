d7fe8b473458e53a7598f32f248b427fe0651934
koyamanX <ckoyama.1996@gmail.com>
2022-02-02 01:49:45 +0900

    [skip ci] WIP: impl itype

--- src/core/instruction_decoder.h
+++ src/core/instruction_decoder.h
@@ -8,18 +8,22 @@ declare instruction_decoder {
 
 	output uops0[7];
 	output uops0_rd[5];
+	output uops0_rs1_valid;
 	output uops0_rs1[5];
+	output uops0_rs2_valid;
 	output uops0_rs2[5];
 	output uops0_imm[32];
-	func_out uops0_alu(uops0, uops0_rd, uops0_rs1, uops0_rs2, uops0_imm);
+	func_out uops0_alu(uops0, uops0_rd, uops0_rs1_valid, uops0_rs1, uops0_rs2_valid, uops0_rs2, uops0_imm);
 	func_out uops0_illegal_instruction();
 
 	output uops1[7];
 	output uops1_rd[5];
+	output uops1_rs1_valid;
 	output uops1_rs1[5];
+	output uops1_rs2_valid;
 	output uops1_rs2[5];
 	output uops1_imm[32];
-	func_out uops1_alu(uops1, uops1_rd, uops1_rs1, uops1_rs2, uops1_imm);
+	func_out uops1_alu(uops1, uops1_rd, uops1_rs1_valid, uops1_rs1, uops1_rs2_valid, uops1_rs2, uops1_imm);
 	func_out uops1_illegal_instruction();
 }
 
