1d8dd6df26fd31d4cef0e91e00733c72847459a5
koyamanX <ckoyama.1996@gmail.com>
2022-02-03 00:24:04 +0900

    [skip ci] Fix

--- src/core/reorder_buffer.h
+++ src/core/reorder_buffer.h
@@ -3,6 +3,7 @@
 
 #define ROB_NUM_OF_ENTRIES 4.0
 #define LOG2_ROB_NUM_OF_ENTRIES _int(_log10(ROB_NUM_OF_ENTRIES)/_log10(2.0))
+#define ROB_TAG_SIZE LOG2_ROB_NUM_OF_ENTRIES+2
 
 struct reorder_buffer_t {
 	Valid0;
@@ -28,8 +29,7 @@ declare reorder_buffer {
 	input pc1[32];
 	input inst1[32];
 	input dest1[5];
-	/* tag start from 0 */
-	output tag[LOG2_ROB_NUM_OF_ENTRIES+1];
+	output tag[ROB_TAG_SIZE];
 	func_in push(valid0, pc0, inst0, dest0, valid1, pc1, inst1, dest1): tag;
 	func_in flush();
 	func_out full();
@@ -38,26 +38,26 @@ declare reorder_buffer {
 	output commit_Dest1[5];
 	output commit_Value1[32];
 	func_out commit(commit_Dest0, commit_Value0, commit_Dest1, commit_Value1);
-	input CDB0Id[LOG2_ROB_NUM_OF_ENTRIES];
+	input CDB0Id[ROB_TAG_SIZE];
 	input CDB0Val[32];
 	func_in CDB0(CDB0Id, CDB0Val);
-	input CDB1Id[LOG2_ROB_NUM_OF_ENTRIES];
+	input CDB1Id[ROB_TAG_SIZE];
 	input CDB1Val[32];
 	func_in CDB1(CDB1Id, CDB1Val);
 	output read_operandA_val[32];
-	input read_operandA_id[LOG2_ROB_NUM_OF_ENTRIES];
+	input read_operandA_id[ROB_TAG_SIZE];
 	output read_operandA_busy;
 	func_in read_operandA(read_operandA_id): read_operandA_busy;
 	output read_operandB_val[32];
-	input read_operandB_id[LOG2_ROB_NUM_OF_ENTRIES];
+	input read_operandB_id[ROB_TAG_SIZE];
 	output read_operandB_busy;
 	func_in read_operandB(read_operandB_id): read_operandB_busy;
 	output read_operandC_val[32];
-	input read_operandC_id[LOG2_ROB_NUM_OF_ENTRIES];
+	input read_operandC_id[ROB_TAG_SIZE];
 	output read_operandC_busy;
 	func_in read_operandC(read_operandC_id): read_operandC_busy;
 	output read_operandD_val[32];
-	input read_operandD_id[LOG2_ROB_NUM_OF_ENTRIES];
+	input read_operandD_id[ROB_TAG_SIZE];
 	output read_operandD_busy;
 	func_in read_operandD(read_operandD_id): read_operandD_busy;
 }
--- src/core/reservation_station.h
+++ src/core/reservation_station.h
@@ -11,19 +11,19 @@ struct reservation_station_t {
 	Op0[7];
 	Vj0[32];
 	Vk0[32];
-	Qj0[LOG2_ROB_NUM_OF_ENTRIES];
-	Qk0[LOG2_ROB_NUM_OF_ENTRIES];
-	Dest0[LOG2_ROB_NUM_OF_ENTRIES];
+	Qj0[ROB_TAG_SIZE];
+	Qk0[ROB_TAG_SIZE];
+	Dest0[ROB_TAG_SIZE];
 	A0[32];
 	Busy1;
 	Op1[7];
 	Vj1[32];
 	Vk1[32];
-	Qj1[LOG2_ROB_NUM_OF_ENTRIES];
-	Qk1[LOG2_ROB_NUM_OF_ENTRIES];
-	Dest1[LOG2_ROB_NUM_OF_ENTRIES];
+	Qj1[ROB_TAG_SIZE];
+	Qk1[ROB_TAG_SIZE];
+	Dest1[ROB_TAG_SIZE];
 	A1[32];
-#define SIZEOF_RESERVATION_STATION_T 220
+#define SIZEOF_RESERVATION_STATION_T 232
 };
 
 declare reservation_station {
@@ -31,35 +31,35 @@ declare reservation_station {
 	input Op0[7];
 	input Vj0[32];
 	input Vk0[32];
-	input Qj0[LOG2_ROB_NUM_OF_ENTRIES];
-	input Qk0[LOG2_ROB_NUM_OF_ENTRIES];
-	input Dest0[LOG2_ROB_NUM_OF_ENTRIES];
+	input Qj0[ROB_TAG_SIZE];
+	input Qk0[ROB_TAG_SIZE];
+	input Dest0[ROB_TAG_SIZE];
 	input A0[32];
 	input Valid1;
 	input Op1[7];
 	input Vj1[32];
 	input Vk1[32];
-	input Qj1[LOG2_ROB_NUM_OF_ENTRIES];
-	input Qk1[LOG2_ROB_NUM_OF_ENTRIES];
-	input Dest1[LOG2_ROB_NUM_OF_ENTRIES];
+	input Qj1[ROB_TAG_SIZE];
+	input Qk1[ROB_TAG_SIZE];
+	input Dest1[ROB_TAG_SIZE];
 	input A1[32];
 	func_in issue(Valid1, Op1, Vj1, Vk1, Qj1, Qk1, Dest1, A1, Valid0, Op0, Vj0, Vk0, Qj0, Qk0, Dest0, A0);
 	func_in flush();
 	func_out full();
 	output dispatch0_Op[7];
-	output dispatch0_Dest[LOG2_ROB_NUM_OF_ENTRIES];
+	output dispatch0_Dest[ROB_TAG_SIZE];
 	output dispatch0_Vj[32];
 	output dispatch0_Vk[32];
 	func_out dispatch0(dispatch0_Op, dispatch0_Dest, dispatch0_Vj, dispatch0_Vk);
 	output dispatch1_Op[7];
-	output dispatch1_Dest[LOG2_ROB_NUM_OF_ENTRIES];
+	output dispatch1_Dest[ROB_TAG_SIZE];
 	output dispatch1_Vj[32];
 	output dispatch1_Vk[32];
 	func_out dispatch1(dispatch1_Op, dispatch1_Dest, dispatch1_Vj, dispatch1_Vk);
-	input CDB0Id[LOG2_ROB_NUM_OF_ENTRIES];
+	input CDB0Id[ROB_TAG_SIZE];
 	input CDB0Val[32];
 	func_in CDB0(CDB0Id, CDB0Val);
-	input CDB1Id[LOG2_ROB_NUM_OF_ENTRIES];
+	input CDB1Id[ROB_TAG_SIZE];
 	input CDB1Val[32];
 	func_in CDB1(CDB1Id, CDB1Val);
 }
