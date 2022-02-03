5284be8a3097d323b3fedb30a03a9c9e4ed000e5
koyamanX <ckoyama.1996@gmail.com>
2022-02-03 23:10:40 +0900

    [skip ci] impl src sel

--- src/core/instruction_decoder.h
+++ src/core/instruction_decoder.h
@@ -1,18 +1,23 @@
 #ifndef INSTRUCTION_DECODER_H
 #define INSTRUCTION_DECODER_H
 
+#define RS_SEL_IMM	2'b00
+#define RS_SEL_REG	2'b01
+#define RS_SEL_PC 	2'b10
+#define RS_SEL_ZERO	2'b11
+
 declare instruction_decoder {
 	input inst[32];
 	func_in decode(inst);
 
 	output uops[7];
 	output uops_rd[5];
-	output uops_rs1_valid;
+	output uops_rs1_sel[2];
 	output uops_rs1[5];
-	output uops_rs2_valid;
+	output uops_rs2_sel[2];
 	output uops_rs2[5];
 	output uops_imm[32];
-	func_out uops_alu(uops, uops_rd, uops_rs1_valid, uops_rs1, uops_rs2_valid, uops_rs2, uops_imm);
+	func_out uops_alu(uops, uops_rd, uops_rs1_sel, uops_rs1, uops_rs2_sel, uops_rs2, uops_imm);
 	func_out uops_illegal_instruction();
 }
 
