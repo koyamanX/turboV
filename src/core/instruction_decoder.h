db1e4410de3b321102438bb26482679009f7ae82
koyamanX <ckoyama.1996@gmail.com>
2022-02-12 00:39:03 +0900

    [skip ci] Add opcode field for ROB

--- src/core/instruction_decoder.h
+++ src/core/instruction_decoder.h
@@ -9,7 +9,7 @@
 declare instruction_decoder {
 	input inst[32];
 	func_in decode(inst);
-
+	output opcode[7];
 	output uops[7];
 	output uops_rd[5];
 	output uops_rs1_sel[2];
@@ -17,7 +17,7 @@ declare instruction_decoder {
 	output uops_rs2_sel[2];
 	output uops_rs2[5];
 	output uops_imm[32];
-	func_out uops_alu(uops, uops_rd, uops_rs1_sel, uops_rs1, uops_rs2_sel, uops_rs2, uops_imm);
+	func_out uops_alu(opcode, uops, uops_rd, uops_rs1_sel, uops_rs1, uops_rs2_sel, uops_rs2, uops_imm);
 	func_out uops_illegal_instruction();
 	func_out jump();
 	func_out branch();
