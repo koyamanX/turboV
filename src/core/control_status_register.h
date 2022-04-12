8800548844f6b7ec9d7d7a7016d408f4266af0c0
koyamanX <ckoyama.1996@gmail.com>
2022-04-12 23:53:41 +0900

    WIP Add CSR module

---
+++ src/core/control_status_register.h
@@ -0,0 +1,20 @@
+#ifndef CONTROL_STATUS_REGISTER_H
+#define CONTROL_STATUS_REGISTER_H
+
+#include "priv.h"
+#include "csr_map.h"
+
+declare control_status_register {
+    input mode[2];
+
+    input csr_rnum[12];
+    output csr_rdata[32];
+    func_in read(csr_rnum): csr_rdata;
+    input wcsr[12];
+    input csr_wnum[12];
+    input csr_wdata[32];
+    func_in write(csr_wnum, csr_wdata);
+    func_out csr_illegal_instruction();
+}
+
+#endif
---
+++ src/core/csr_map.h
@@ -0,0 +1,6 @@
+#ifndef CSR_MAP_H
+#define CSR_MAP_H
+
+#define MISA            0x301
+
+#endif
