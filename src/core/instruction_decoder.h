a453c6ddb8a3847cf4dc9f0c25caa19bb02a49a2
koyamanX <ckoyama.1996@gmail.com>
2022-04-17 23:30:50 +0900

    Impl CSR insts with uimm

--- src/core/instruction_decoder.h
+++ src/core/instruction_decoder.h
@@ -2,7 +2,7 @@
 #define INSTRUCTION_DECODER_H
 
 #define RS1_SEL_REG     2'b00
-#define RS1_SEL_IMM     2'b01
+#define RS1_SEL_UIMM    2'b01
 #define RS1_SEL_PC      2'b10
 
 #define RS2_SEL_REG     2'b00
