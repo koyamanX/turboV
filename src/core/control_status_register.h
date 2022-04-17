01cc1976bdd979b78dd1ca8e4618d1332c1231ca
koyamanX <ckoyama.1996@gmail.com>
2022-04-18 00:08:14 +0900

    Impl illegal instruction exceptions on CSR access

--- src/core/control_status_register.h
+++ src/core/control_status_register.h
@@ -11,7 +11,8 @@ declare control_status_register {
     input csr_wnum[12];
     input csr_wdata[32];
     func_in write(csr_wnum, csr_wdata);
-    func_out csr_illegal_instruction();
+    func_out csr_read_illegal_instruction();
+    func_out csr_write_illegal_instruction();
     func_in reset();
 
     input trap_cause[32];
