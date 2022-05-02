133b25300109eb97c6ff17ac487bf814f20ec75f
koyamanX <ckoyama.1996@gmail.com>
2022-05-03 03:16:25 +0900

    fix opcode for mem_misc

--- src/core/opcode_map.h
+++ src/core/opcode_map.h
@@ -5,7 +5,7 @@
 #define STORE           7'b0100011
 #define BRANCH          7'b1100011
 #define JALR            7'b1100111
-#define MISC_MEM        7'b1101111
+#define MISC_MEM        7'b0001111
 #define AMO             7'b0101111
 #define JAL             7'b1101111
 #define OP_IMM          7'b0010011
