b0e551c53e7854bc76c18e8a870f20dd79c70fb4
koyamanX <ckoyama.1996@gmail.com>
2022-05-30 21:38:48 +0900

    Fix timer interrupt and add test

--- tests/utils/csr.h
+++ tests/utils/csr.h
@@ -1,5 +1,6 @@
 #ifndef CSR_H
 #define CSR_H
+#include "types.h"
 
 #define def_csr_read_func(csrname) static inline uint32_t csrname##_read(void) { uint32_t x; asm volatile("csrr %0, " #csrname ";" : "=r"(x)); return x;}
 #define def_csr_write_func(csrname) static inline void csrname##_write(uint32_t x) {asm volatile("csrw " #csrname ", %0;" :: "r"(x));}
