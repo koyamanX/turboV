2c39ab95b81d33aac65cdee25b461680561b3a0f
koyamanX <ckoyama.1996@gmail.com>
2022-06-02 00:53:25 +0900

    Fix exit failure code

--- tests/utils/sim_exit.h
+++ tests/utils/sim_exit.h
@@ -2,8 +2,8 @@
 #define SIM_EXIT_H
 
 #define SIM_EXIT ((volatile unsigned int *)(0x80001000))
-#define SIM_EXIT_FAILURE (!1)
-#define SIM_EXIT_SUCCESS (!0)
+#define SIM_EXIT_FAILURE (2)
+#define SIM_EXIT_SUCCESS (1)
 
 void sim_exit(int status) {
     *SIM_EXIT = status;
