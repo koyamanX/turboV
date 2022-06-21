632bdf17850e054fbc76a3128d0e79b816ede565
koyamanX <ckoyama.1996@gmail.com>
2022-06-22 00:09:21 +0900

    Change uop mapping

--- src/core/uops.h
+++ src/core/uops.h
@@ -3,7 +3,7 @@
 
 #define uOP_SIZE                        7
 
-/* uOps for ALU */
+#define uOP_ALU                         3'b000
 #define uOP_ALU_ADD                     7'b000_0000
 #define uOP_ALU_SUB                     7'b000_1000
 #define uOP_ALU_SLL                     7'b000_0001
@@ -15,32 +15,33 @@
 #define uOP_ALU_OR                      7'b000_0110
 #define uOP_ALU_AND                     7'b000_0111
 
-#define uOP_BRU_BEQ                     7'b000_0000
-#define uOP_BRU_BNE                     7'b000_0001
-#define uOP_BRU_BLT                     7'b000_0100
-#define uOP_BRU_BGE                     7'b000_0101
-#define uOP_BRU_BLTU                    7'b000_0110
-#define uOP_BRU_BGEU                    7'b000_0111
-#define uOP_BRU_JALR                    7'b000_1111
+#define uOP_BRU                         3'b001
+#define uOP_BRU_BEQ                     7'b001_0000
+#define uOP_BRU_BNE                     7'b001_0001
+#define uOP_BRU_BLT                     7'b001_0100
+#define uOP_BRU_BGE                     7'b001_0101
+#define uOP_BRU_BLTU                    7'b001_0110
+#define uOP_BRU_BGEU                    7'b001_0111
+#define uOP_BRU_JALR                    7'b001_1111
 
+#define uOP_LSU                         3'b010
+#define uOP_LSU_LOAD_WORD               7'b010_0010
+#define uOP_LSU_LOAD_HALFWORD           7'b010_0001
+#define uOP_LSU_LOAD_BYTE               7'b010_0000
+#define uOP_LSU_LOAD_HALFWORD_UNSIGNED  7'b010_0101
+#define uOP_LSU_LOAD_BYTE_UNSIGNED      7'b010_0100
+#define uOP_LSU_STORE_WORD              7'b010_1010
+#define uOP_LSU_STORE_HALFWORD          7'b010_1001
+#define uOP_LSU_STORE_BYTE              7'b010_1000
+
+#define uOP_SYSTEM                      3'b011
 #define uOP_SYSTEM_CSRRW                7'b110_0110
 #define uOP_SYSTEM_CSRRS                7'b110_0110
 #define uOP_SYSTEM_CSRRC                7'b110_1111
 #define uOP_SYSTEM_CSRRS_NO_UPDATE_CSR  7'b010_0110
 #define uOP_SYSTEM_CSRRC_NO_UPDATE_CSR  7'b010_1111
-
 #define uOP_SYSTEM_MRET                 7'b001_1111
 #define uOP_SYSTEM_ECALL                7'b001_1100
 #define uOP_SYSTEM_EBREAK               7'b001_1110
-
-#define uOP_LSU_LOAD_WORD               7'b000_0010
-#define uOP_LSU_LOAD_HALFWORD           7'b000_0001
-#define uOP_LSU_LOAD_BYTE               7'b000_0000
-#define uOP_LSU_LOAD_HALFWORD_UNSIGNED  7'b000_0101
-#define uOP_LSU_LOAD_BYTE_UNSIGNED      7'b000_0100
-
-#define uOP_LSU_STORE_WORD              7'b000_1010
-#define uOP_LSU_STORE_HALFWORD          7'b000_1001
-#define uOP_LSU_STORE_BYTE              7'b000_1000
 
 #endif
