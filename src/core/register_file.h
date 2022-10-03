33f88ebb216e00c6b95316bf678590ebb77bce5b
koyamanX <ckoyama.1996@gmail.com>
2022-10-04 00:55:41 +0900

    WIP expand register file size

--- src/core/register_file.h
+++ src/core/register_file.h
@@ -3,22 +3,22 @@
 #include "riscv.h"
 
 declare register_file {
-    input rnumA[5];
+    input rnumA[6];
     output rdataA[XLEN];
     func_in readA(rnumA): rdataA;
-    input rnumB[5];
+    input rnumB[6];
     output rdataB[XLEN];
     func_in readB(rnumB): rdataB;
-    input rnumC[5];
+    input rnumC[6];
     output rdataC[XLEN];
     func_in readC(rnumC): rdataC;
-    input rnumD[5];
+    input rnumD[6];
     output rdataD[XLEN];
     func_in readD(rnumD): rdataD;
-    input wnumA[5];
+    input wnumA[6];
     input wdataA[XLEN];
     func_in writeA(wnumA, wdataA);
-    input wnumB[5];
+    input wnumB[6];
     input wdataB[XLEN];
     func_in writeB(wnumB, wdataB);
 }
