848c63c14b42e19dcead03a53d321e20c919e4dd
koyamanX <ckoyama.1996@gmail.com>
2022-10-07 22:16:44 +0900

    WIP remove unused modules

--- src/core/load_store_unit.h
+++ src/core/load_store_unit.h
@@ -2,21 +2,22 @@
 #define LOAD_STORE_UNIT_H
 
 #include "wishbone_common.h"
-#include "reorder_buffer.h"
 
+// TODO:
+#define N 2
 declare load_store_unit {
     input issue_A[32];
     input issue_uOp[7];
     input issue_Vj[32];
-    input issue_RobId[ROB_TAG_SIZE];
+    input issue_RobId[N];
     func_in issue(issue_A, issue_uOp, issue_Vj, issue_RobId);
     func_out stall();
     func_out full();
     func_in flush();
-    output CDBOutId[ROB_TAG_SIZE];
+    output CDBOutId[N];
     output CDBOutVal[32];
     func_out CDBOut(CDBOutId, CDBOutVal);
-    input commitId[ROB_TAG_SIZE];
+    input commitId[N];
     func_in commit(commitId);
 
     WISHBONE_GEN_MASTER_IF(32, 64, 8)
