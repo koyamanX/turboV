e16f6c630b365aebc3a2086f35902648f966c7de
koyamanX <ckoyama.1996@gmail.com>
2022-10-14 02:08:44 +0900

    WIP Impl rob issue

--- src/core/reorder_buffer.h
+++ src/core/reorder_buffer.h
@@ -9,15 +9,15 @@ struct reorder_buffer_t {
 	uop0[SIZEOF_UOP_T];
 	dreg0[5];
 	ppreg0[6];
-	exception0;
+	cause0[SIZEOF_CAUSE_T];
 	retired0;
 	valid1;
 	uop1[SIZEOF_UOP_T];
 	dreg1[5];
 	ppreg1[6];
-	exception1;
+	cause1[SIZEOF_CAUSE_T];
 	retired1;
-#define SIZEOF_REORDER_BUFFER_T 57+SIZEOF_UOP_T+SIZEOF_UOP_T
+#define SIZEOF_REORDER_BUFFER_T 57+SIZEOF_UOP_T+SIZEOF_UOP_T+SIZEOF_CAUSE_T+SIZEOF_CAUSE_T
 };
 
 #define NUMBER_OF_REORDER_BUFFER 8
@@ -25,9 +25,20 @@ struct reorder_buffer_t {
 #define LOG2_REORDER_BUFFER_NUM_OF_ENTRIES _int(_log10(REORDER_BUFFER_NUM_OF_ENTRIES)/_log10(2.0))
 
 declare reorder_buffer {
-	input new_entry[SIZEOF_REORDER_BUFFER_T];
+	input PC[29];
+	input valid0;
+	input uop0[SIZEOF_UOP_T];
+	input dreg0[5];
+	input ppreg0[6];
+	input cause0[SIZEOF_CAUSE_T];
+	input valid1;
+	input uop1[SIZEOF_UOP_T];
+	input dreg1[5];
+	input ppreg1[6];
+	input cause1[SIZEOF_CAUSE_T];
 	output ptr[LOG2_REORDER_BUFFER_NUM_OF_ENTRIES+1];
-	func_in issue(new_entry): ptr;
+	func_in issue(PC, valid0, uop0, dreg0, ppreg0, cause0,
+					  valid1, uop1, dreg1, ppreg1, cause1): ptr;
 	output commit_entry[SIZEOF_REORDER_BUFFER_T];
 	func_in commit(): commit_entry;
 	func_out commitable();
