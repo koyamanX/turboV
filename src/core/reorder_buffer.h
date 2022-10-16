49e1291485a9f67928fe53575f06d43741201a92
koyamanX <ckoyama.1996@gmail.com>
2022-10-16 21:26:34 +0900

    WIP Impl retirement RMT [skip ci]

--- src/core/reorder_buffer.h
+++ src/core/reorder_buffer.h
@@ -8,19 +8,21 @@ struct reorder_buffer_t {
 	valid0;
 	uop0[SIZEOF_UOP_T];
 	dreg0[5];
+	preg0[6];
 	ppreg0[6];
 	cause0[SIZEOF_CAUSE_T];
 	retired0;
 	valid1;
 	uop1[SIZEOF_UOP_T];
 	dreg1[5];
+	preg1[6];
 	ppreg1[6];
 	cause1[SIZEOF_CAUSE_T];
 	retired1;
 #ifdef ENABLE_DEBUG
 	inst0[32];
 	inst1[32];
-#define SIZEOF_REORDER_BUFFER_T 55+SIZEOF_UOP_T+SIZEOF_UOP_T+SIZEOF_CAUSE_T+SIZEOF_CAUSE_T+32+32
+#define SIZEOF_REORDER_BUFFER_T 55+SIZEOF_UOP_T+SIZEOF_UOP_T+SIZEOF_CAUSE_T+SIZEOF_CAUSE_T+32+32+12
 #else
 #define SIZEOF_REORDER_BUFFER_T 55+SIZEOF_UOP_T+SIZEOF_UOP_T+SIZEOF_CAUSE_T+SIZEOF_CAUSE_T
 #endif
@@ -36,22 +38,24 @@ declare reorder_buffer {
 	input valid0;
 	input uop0[SIZEOF_UOP_T];
 	input dreg0[5];
+	input preg0[6];
 	input ppreg0[6];
 	input cause0[SIZEOF_CAUSE_T];
 	input valid1;
 	input uop1[SIZEOF_UOP_T];
 	input dreg1[5];
+	input preg1[6];
 	input ppreg1[6];
 	input cause1[SIZEOF_CAUSE_T];
 	output ptr[REORDER_BUFFER_PTR_SIZE];
 #ifdef ENABLE_DEBUG
 	input inst0[32];
 	input inst1[32];
-	func_in issue(PC, valid0, uop0, dreg0, ppreg0, cause0,
-					  valid1, uop1, dreg1, ppreg1, cause1, inst0, inst1): ptr;
+	func_in issue(PC, valid0, uop0, dreg0, preg0, ppreg0, cause0,
+					  valid1, uop1, dreg1, preg1, ppreg1, cause1, inst0, inst1): ptr;
 #else
-	func_in issue(PC, valid0, uop0, dreg0, ppreg0, cause0,
-					  valid1, uop1, dreg1, ppreg1, cause1): ptr;
+	func_in issue(PC, valid0, uop0, dreg0, preg0, ppreg0, cause0,
+					  valid1, uop1, dreg1, preg1, ppreg1, cause1): ptr;
 
 #endif
 	output commit_entry[SIZEOF_REORDER_BUFFER_T];
