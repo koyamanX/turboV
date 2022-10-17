daaab39f81733839fbf4dc9824350c4c26528302
koyamanX <ckoyama.1996@gmail.com>
2022-10-17 23:16:30 +0900

    WIP rename signals [skip ci]

--- src/core/recovery_manager.h
+++ src/core/recovery_manager.h
@@ -13,15 +13,15 @@ struct recovery_manager_t {
 declare recovery_manager {
 	input head[REORDER_BUFFER_PTR_SIZE];
 #define PTR2AGE(ptr) (if(ptr<head) ptr+NUMBER_OF_REORDER_BUFFER*2 else ptr)
-	input retire0_ptr[REORDER_BUFFER_PTR_SIZE];
-	input retire0_entry[SIZEOF_RECOVERY_MANAGER_T];
-	func_in retire0(retire0_ptr, retire0_entry);
-	input retire1_ptr[REORDER_BUFFER_PTR_SIZE];
-	input retire1_entry[SIZEOF_RECOVERY_MANAGER_T];
-	func_in retire1(retire1_ptr, retire1_entry);
-	input retire2_ptr[REORDER_BUFFER_PTR_SIZE];
-	input retire2_entry[SIZEOF_RECOVERY_MANAGER_T];
-	func_in retire2(retire2_ptr, retire2_entry);
+	input complete0_ptr[REORDER_BUFFER_PTR_SIZE];
+	input complete0_entry[SIZEOF_RECOVERY_MANAGER_T];
+	func_in complete0(complete0_ptr, complete0_entry);
+	input complete1_ptr[REORDER_BUFFER_PTR_SIZE];
+	input complete1_entry[SIZEOF_RECOVERY_MANAGER_T];
+	func_in complete1(complete1_ptr, complete1_entry);
+	input complete2_ptr[REORDER_BUFFER_PTR_SIZE];
+	input complete2_entry[SIZEOF_RECOVERY_MANAGER_T];
+	func_in complete2(complete2_ptr, complete2_entry);
 	input commit0_ptr[REORDER_BUFFER_PTR_SIZE];
 	output commit0_trap;
 	func_in commit0(commit0_ptr): commit0_trap;
