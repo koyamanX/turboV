575b64dd2b7ed5ce6237c13e01a2b07fb150a837
koyamanX <ckoyama.1996@gmail.com>
2022-10-25 02:12:49 +0900

    WIP fix for riscv-tests [skip ci]

--- src/core/load_store_unit.h
+++ src/core/load_store_unit.h
@@ -5,19 +5,20 @@
 #include "reorder_buffer.h"
 
 declare load_store_unit {
-    input issue_uOp[uOP_SIZE];
+    input issue_uop[SIZEOF_UOP_T];
     input issue_addr[32];
     input issue_wdata[32];
 	input issue_prd[6];
     input issue_ptr[REORDER_BUFFER_PTR_SIZE];
-    func_in issue(issue_ptr, issue_prd, issue_uOp, issue_addr, issue_wdata);
+    func_in issue(issue_ptr, issue_prd, issue_uop, issue_addr, issue_wdata);
     func_out stall();
     func_out full();
     func_in flush();
     output writeback_ptr[REORDER_BUFFER_PTR_SIZE];
+    output writeback_uop[SIZEOF_UOP_T];
     output writeback_prd[6];
     output writeback_data[32];
-    func_out writeback(writeback_ptr, writeback_prd, writeback_data);
+    func_out writeback(writeback_ptr, writeback_uop, writeback_prd, writeback_data);
     input commit_ptr[REORDER_BUFFER_PTR_SIZE];
     func_in commit(commit_ptr);
 
