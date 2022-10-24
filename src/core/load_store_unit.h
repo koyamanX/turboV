c6533d39a375ee55d1a4363496804b519635dc9f
koyamanX <ckoyama.1996@gmail.com>
2022-10-25 01:28:41 +0900

    WIP add LSU and fix ROB [skip ci]

--- src/core/load_store_unit.h
+++ src/core/load_store_unit.h
@@ -2,23 +2,24 @@
 #define LOAD_STORE_UNIT_H
 
 #include "wishbone_common.h"
+#include "reorder_buffer.h"
 
-// TODO:
-#define N 2
 declare load_store_unit {
-    input issue_A[32];
-    input issue_uOp[7];
-    input issue_Vj[32];
-    input issue_RobId[N];
-    func_in issue(issue_A, issue_uOp, issue_Vj, issue_RobId);
+    input issue_uOp[uOP_SIZE];
+    input issue_addr[32];
+    input issue_wdata[32];
+	input issue_prd[6];
+    input issue_ptr[REORDER_BUFFER_PTR_SIZE];
+    func_in issue(issue_ptr, issue_prd, issue_uOp, issue_addr, issue_wdata);
     func_out stall();
     func_out full();
     func_in flush();
-    output CDBOutId[N];
-    output CDBOutVal[32];
-    func_out CDBOut(CDBOutId, CDBOutVal);
-    input commitId[N];
-    func_in commit(commitId);
+    output writeback_ptr[REORDER_BUFFER_PTR_SIZE];
+    output writeback_prd[6];
+    output writeback_data[32];
+    func_out writeback(writeback_ptr, writeback_prd, writeback_data);
+    input commit_ptr[REORDER_BUFFER_PTR_SIZE];
+    func_in commit(commit_ptr);
 
     WISHBONE_GEN_MASTER_IF(32, 64, 8)
 }
