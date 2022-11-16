adab8af40a839dd31688ff7989e853619445b6ea
koyamanX <ckoyama.1996@gmail.com>
2022-11-16 22:45:42 +0900

    WIP

--- tools/json2kanata/main.cpp
+++ tools/json2kanata/main.cpp
@@ -17,23 +17,99 @@ int main(int argc, char **argv) {
 		std::cin >> j >> std::ws;
 
 		std::cout << "C\t1" << std::endl;
+		if(!j["stages"]["dispatch0"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["dispatch0"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds0" << std::endl;
+		}
+		if(!j["stages"]["dispatch1"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["dispatch1"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds1" << std::endl;
+		}
+		if(!j["stages"]["dispatch2"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["dispatch2"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds2" << std::endl;
+		}
+		if(!j["stages"]["execute0"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["execute0"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds0" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex0" << std::endl;
+		}
+		if(!j["stages"]["execute1"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["execute1"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds1" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex1" << std::endl;
+		}
+		if(!j["stages"]["execute2"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["execute2"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ds2" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex2" << std::endl;
+		}
+		if(!j["stages"]["writeback0"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["writeback0"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex0" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Wb0" << std::endl;
+		}
+		if(!j["stages"]["writeback1"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["writeback1"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex1" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Wb1" << std::endl;
+		}
+		if(!j["stages"]["writeback2"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["writeback2"]["ptr"];
+			std::cout << "E" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Ex2" << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Wb2" << std::endl;
+		}
+		if(!j["stages"]["commit0"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["commit0"]["ptr"];
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Cm0" << std::endl;
+		}
+		if(!j["stages"]["commit1"].is_null()) {
+			std::string ptr;
+
+			ptr = j["stages"]["commit1"]["ptr"];
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Cm1" << std::endl;
+		}
 		if(!j["stages"]["rename0"].is_null()) {
 			std::string ptr;
 
 			ptr = j["stages"]["rename0"]["ptr"];
-			ptr2id[stoi(ptr)] = id++;
-			std::cout << "I" << "\t" << ptr2id[stoi(ptr)] << "\t" << ptr << "\t" << tid << std::endl;
-			std::cout << "S" << "\t" << ptr2id[stoi(ptr)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			ptr2id[stoi(ptr, nullptr, 16)] = id++;
+			std::cout << "I" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << ptr << "\t" << tid << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			std::cout << "L" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 1 << "\t" << "ptr: " << j["stages"]["rename0"]["ptr"] << std::endl;
 		}
 		if(!j["stages"]["rename1"].is_null()) {
 			std::string ptr;
 
 			ptr = j["stages"]["rename1"]["ptr"];
-			ptr2id[stoi(ptr)] = id++;
-			std::cout << "I" << "\t" << ptr2id[stoi(ptr)] << "\t" << ptr << "\t" << tid << std::endl;
-			std::cout << "S" << "\t" << ptr2id[stoi(ptr)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			ptr2id[stoi(ptr, nullptr, 16)] = id++;
+			std::cout << "I" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << ptr << "\t" << tid << std::endl;
+			std::cout << "S" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 0 << "\t" << "Rn" << std::endl;
+			std::cout << "L" << "\t" << ptr2id[stoi(ptr, nullptr, 16)] << "\t" << 1 << "\t" << "ptr: " << j["stages"]["rename1"]["ptr"] << std::endl;
 		}
     }
-
     return 0;
 }
