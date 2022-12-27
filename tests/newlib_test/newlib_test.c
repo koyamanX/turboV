111466709c8f4fd4fe93de48e48f5025080ae209
koyamanX <ckoyama.1996@gmail.com>
2022-12-27 22:51:22 +0900

    Add UART Tx driver

--- tests/newlib_test/newlib_test.c
+++ tests/newlib_test/newlib_test.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#define SIM_EXIT ((volatile unsigned int *) 0x80001000)
 
 int main(void) {
 	int *p;
+
+	printf("%s\n", "Hello,world!");
+
+	for(int i = 0; i < 1024; i++)
+		asm volatile("nop");
 	
 	p = malloc(sizeof(int));
 
 	*p = 1;
 
-	*((unsigned int *) 0x80001000) = *p;
+	*SIM_EXIT = *p;
 
 	exit(EXIT_FAILURE);
 }
