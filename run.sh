f78de4f9afd06e02de5f9e790f4ac8631258aa71
koyamanX <ckoyama.1996@gmail.com>
2023-01-01 16:03:36 +0900

    Mount intel fpga tools

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker run -it --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     --privileged \
     -v /sys:/sys:ro \
-    -v /opt/intelFPGA_lite/21.1:/opt/intelFPGA_lite/latest \
+    -v /opt/intelFPGA_lite/22.1std:/opt/intelFPGA_lite/latest \
 	-v $(pwd):/work \
 	-w /work \
 	koyamanx/turbov_dev:v1.4 $1
