5b0f09314dcc51cc3911a032427adafcaad87dc2
Chihiro Koyama <36282396+koyamanX@users.noreply.github.com>
2023-01-03 23:36:39 +0900

    Revert "Impl UART Tx for simulation"

--- software/libgloss/uart.c
+++ software/libgloss/uart.c
@@ -2,12 +2,7 @@
 #include "uart.h"
 
 int uart_putchar(int ch) {
-	*UART_TXCTRL = 0x1;
-	while(*UART_TXDATA & 0x80000000)
-		asm volatile("nop");
-	*UART_TXDATA = (char)ch;
 
-	return (unsigned char )ch;
 }
 int uart_getchar(void) {
 
--- software/libgloss/uart.h
+++ software/libgloss/uart.h
@@ -1,14 +1,6 @@
 #ifndef UART_H
 #define UART_H
 
-#define UART_TXDATA ((volatile unsigned int *)0x40000000)
-#define UART_RXDATA ((volatile unsigned int *)0x40000004)
-#define UART_TXCTRL ((volatile unsigned int *)0x40000008)
-#define UART_RXCTRL ((volatile unsigned int *)0x4000000c)
-#define UART_IE 	((volatile unsigned int *)0x40000010)
-#define UART_IP 	((volatile unsigned int *)0x40000014)
-#define UART_DIV 	((volatile unsigned int *)0x40000018)
-
 int uart_putchar(int ch);
 int uart_getchar(void);
 
